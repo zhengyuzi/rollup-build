fbbc2ef5b69237c812f600697d6c79fb4fe4f8fd
zhengyuzi <381740310@qq.com>
2024-09-19 14:41:32 +0800

    fix: added error message for missing ```build.config.ts```

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ import type {
 } from './types'
 import process from 'node:process'
 import { loadConfig } from 'c12'
+import consola from 'consola'
 
 export const EXIT_CODE = {
   SUCCESS: 0,
@@ -27,6 +28,11 @@ export async function loadBuildConfig(cwd = process.cwd()) {
     name: 'build',
     cwd,
   })
+
+  if (!config.source) {
+    consola.error(`No build.config.ts file found at ${cwd}`)
+    process.exit(EXIT_CODE.FATALEXCEPTION)
+  }
 
   return config.source.map((item) => {
     return { ...defaultConfig, ...item }
